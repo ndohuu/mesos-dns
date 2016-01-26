4608b597a1a01703f5dcf77d6d0cf0bc4a34a4a8
ndohuu <nicolas.dohuu@gmail.com>
2016-01-26 18:35:44 +0100

    quote error

--- start-mesos-dns.sh
+++ start-mesos-dns.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i s#ZOOKEEPER_MESOS#$ZOOKEEPER_MESOS# /config.json
-sed -i s#MASTERS#"$MASTERS"# /config.json
+sed -i s#MASTERS#$MASTERS# /config.json
 sed -i s#DOMAIN#$DOMAIN# /config.json
 sed -i s#RESOLVERS#$RESOLVERS# /config.json
 
